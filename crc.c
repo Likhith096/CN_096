a9041eee044c6e97f5b32e34e141c91438424290
Likhith096 <117436338+Likhith096@users.noreply.github.com>
2023-08-16 15:05:11 +0530

    Add files via upload

--- crc.c
+++ crc.c
@@ -5,10 +5,10 @@
 #define CRC_POLY 0x1021
 
 // Function to calculate CRC-CCITT checksum
-unsigned short calculateCRC(const char *data, int length) {
-    unsigned short crc = 0xFFFF; // Initial value
+int calculateCRC(const char *data, int length) {
+    int crc = 0xFFFF; // Initial value
     for (int i = 0; i < length; i++) {
-        crc ^= (unsigned short)data[i] << 8;
+        crc ^= (int)data[i] << 8;
         for (int j = 0; j < 8; j++) {
             if (crc & 0x8000)
                 crc = (crc << 1) ^ CRC_POLY;
@@ -21,22 +21,26 @@ unsigned short calculateCRC(const char *data, int length) {
 
 int main() {
     char data[100]; // Replace with your actual data
+
+    // Input data from the user
     printf("Enter data: ");
     scanf("%s", data);
 
     int dataLength = strlen(data);
-    unsigned short checksum = calculateCRC(data, dataLength);
+    int checksum = calculateCRC(data, dataLength);
 
+    // Display calculated CRC
     printf("Calculated CRC: 0x%04X\n", checksum);
 
     // Simulating error by changing a bit
     // data[2] ^= 0x01; // Uncomment this line to introduce an error
 
     // Verify the received data
-    unsigned short receivedChecksum;
+    int receivedChecksum;
     printf("Enter received CRC: ");
-    scanf("%hx", &receivedChecksum);
+    scanf("%x", &receivedChecksum);
 
+    // Compare received CRC with calculated CRC
     if (receivedChecksum == checksum) {
         printf("Data is error-free.\n");
     } else {
